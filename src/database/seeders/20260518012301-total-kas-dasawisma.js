66cdb77eabfb7556b4a14c2345a0c9509a438fc0
unknown <rafif.purnomo@gmail.com>
2026-07-09 15:27:51 +0700

    update seeders

--- src/database/seeders/20260518012301-total-kas-dasawisma.js
+++ src/database/seeders/20260518012301-total-kas-dasawisma.js
@@ -2,26 +2,15 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const now = new Date();
-    const saldoPerRw = {
-      1: 85000,
-      2: 85000,
-      3: 100000,
-      4: 75000,
-      5: 125000,
-      6: 50000,
-    };
+    const data = [];
 
-    const data = Array.from({ length: 10 }, (_, index) => {
-      const rwId = index + 1;
-
-      return {
+    for (let rwId = 1; rwId <= 10; rwId++) {
+      data.push({
         rw_id: rwId,
-        jumlah_keseluruhan: saldoPerRw[rwId] ?? 0,
-        updated_at: now,
-      };
-    });
-
+        jumlah_keseluruhan: 0,
+        updated_at: null
+      });
+    }
     await queryInterface.bulkInsert("total_kas_dasawisma", data);
   },
 
