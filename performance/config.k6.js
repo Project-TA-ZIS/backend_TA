cb50fe55fb9c6ef04e3539a0c5c2fab80fadd612
unknown <rafif.purnomo@gmail.com>
2026-07-14 00:26:39 +0700

    enhance CORS configuration to allow dynamic origins based on environment

--- performance/config.k6.js
+++ performance/config.k6.js
@@ -1,5 +1,10 @@
 export const stages = () => {
   return [
+    // { duration: "10s", target: 10 }, // ramp-up ke 10 user
+    // { duration: "1m", target: 50 }, // naik ke 50 user
+    // { duration: "1m", target: 100 }, // naik ke 100 user
+    // { duration: "30s", target: 0 }, // ramp-down
+    
     { duration: "1m", target: 10 },
     { duration: "1m", target: 25 },
     { duration: "1m", target: 50 },
