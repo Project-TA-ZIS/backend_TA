b5ab303b3956072e75d2b2a3bc279e4e59ceacf9
unknown <rafif.purnomo@gmail.com>
2026-07-17 20:48:10 +0700

    adding seeders

--- src/database/seeders/20260518012301-total-kas-dasawisma.js
+++ src/database/seeders/20260518012301-total-kas-dasawisma.js
@@ -5,12 +5,18 @@ module.exports = {
     const data = [];
 
     for (let rwId = 1; rwId <= 10; rwId++) {
+      let total = 0;
+
+      if (rwId === 1) total = 275000;
+      if (rwId === 2) total = 250000;
+
       data.push({
         rw_id: rwId,
-        jumlah_keseluruhan: 0,
-        updated_at: null
+        jumlah_keseluruhan: total,
+        updated_at: null,
       });
     }
+
     await queryInterface.bulkInsert("total_kas_dasawisma", data);
   },
 
--- src/database/seeders/20260518012302-seed-total-zis.js
+++ src/database/seeders/20260518012302-seed-total-zis.js
@@ -5,17 +5,17 @@ module.exports = {
     await queryInterface.bulkInsert("total_zis", [
       {
         kategori: "zakat maal",
-        jumlah_keseluruhan: 0,
+        jumlah_keseluruhan: 5150000,
         updated_at: null,
       },
       {
         kategori: "shodaqoh",
-        jumlah_keseluruhan: 0,
+        jumlah_keseluruhan: 50000,
         updated_at: null,
       },
       {
         kategori: "infaq",
-        jumlah_keseluruhan: 0,
+        jumlah_keseluruhan: 400000,
         updated_at: null,
       },
     ]);
